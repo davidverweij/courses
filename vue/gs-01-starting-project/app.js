6930f53d4e3d90fb2b6dc8f62243c9015b7b3d47
David Verweij <davidverweij@gmail.com>
2020-10-27 11:07:23 +0100

    finished VUE demo

--- vue/gs-01-starting-project/app.js
+++ vue/gs-01-starting-project/app.js
@@ -8,12 +8,14 @@ Vue.createApp({
   methods: {
     addGoal() {
       this.goals.push(this.enteredValue);
+      this.enteredValue = '';
     }
   }
 }).mount('#app');
 
 
-
+//
+//
 // const buttonEl = document.querySelector('button');
 // const inputEl = document.querySelector('input');
 // const listEl = document.querySelector('ul');
